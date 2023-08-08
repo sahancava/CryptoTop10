3aa7e78b418ef0dda8226341e31c2ba99ff557e4
Şahan <57447079+sahancava@users.noreply.github.com>
2023-08-08 23:00:49 +0300

    Initial Commit

---
+++ CryptoTop10/CryptoTop10App.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTop10App.swift
+//  CryptoTop10
+//
+//  Created by sahanc on 8.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTop10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
